e61687b582e02c42ed3e0c3c35cee3a2a953b86b
mian <96155069+mianafzaal@users.noreply.github.com>
2022-12-04 18:40:04 +0500

    Update config.js

--- config.js
+++ config.js
@@ -14,33 +14,33 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['923430436852'] //ur owner number
+global.ownername = "مᷠــᷧــͥــⷨـــᷟــͥــیاں⃟💌" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
+global.owner = ['923430436852'] //ur number
+global.ownernomer = "923430436852" //ur number
+global.ownertag = '923430436852' //ur tag number
+global.premium = ['923430436852'] //ur premium number
+global.botname = 'Mian Bot V5' //ur bot name
+global.ownername = "مᷠــᷧــͥــⷨـــᷟــͥــیاں⃟💌"
+global.linkz = "https://instagram.com/bp_bts_003?igshid=YmMyMTA2M2Y=" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/bp_bts_003?igshid=YmMyMTA2M2Y=" //ur website to be displayed
+global.botscript = 'https://instagram.com/bp_bts_003?igshid=YmMyMTA2M2Y=' //script link
+global.themeemoji = "😘" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "Mian Bot M💗T" //ur sticker watermark author
+global.wm = "Mian Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
 global.prefa = ['','!','.','#','&']
 global.sp = ''
 global.mess = {
-    success: 'Done✓',
+    success: 'Done✓😁',
     admin: 'This feature could be used by admins only!',
     botAdmin: 'Bot Must Be Admin First!',
     premime: 'Premium Special Features If You Want to Register Type Rent',
@@ -48,7 +48,7 @@ global.mess = {
     group: 'Features Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This feature could be used by bot only',
-    wait: 'In process...',
+    wait: 'In process😲...',
     linkm: 'Where is the link?',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
